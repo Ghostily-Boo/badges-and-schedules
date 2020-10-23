00517eb4a766300c3a6b7d4672506b7b00ba085b
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-10-23 16:31:14 -0400

    commit tree 0feadaee4323ff8b64cd1addba526cafc59f86e5

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map {|array| badge_maker(array)}
+end
+
+def assign_rooms(speakers)
+    array = []
+    speakers.each_with_index do |speakers, index|
+        index += 1
+        array << "Hello, #{speakers}! You'll be assigned to room #{index}!"
+    end
+    array
+end
+
+def printer(people)
+    badges = batch_badge_creator(people)
+    rooms = assign_rooms(people)
+    badges.each {|badges| puts "#{badges}"}
+    rooms.each {|rooms| puts "#{rooms}"}
+end
